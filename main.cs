5b5d846f39082e1967e4511d51efeea4a72815d1
GEVELAND <42362153+EvelAndG@users.noreply.github.com>
2021-09-20 16:38:11 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    
+    double totalCost = 100.2786753;
+
+
+    Console.WriteLine(String.Format("{0:C}", totalCost));
+
+    Console.WriteLine(String.Format("{0:F2}", totalCost));
+		
+	Card myObj = new Card("bob", "bob", 5);
+	Console.WriteLine(myObj.name);
+	}
+
+
+
+}
---
+++ resources
@@ -0,0 +1,19 @@
+public class Card {
+	public string name;
+	public string type;
+	public int number;
+
+	public Card(string cardName, string cardType, int cardNumber)
+	{
+		name = cardName;
+		type = cardType;
+		number = cardNumber;
+	}
+	
+
+
+
+
+ 
+
+}
